3838b00dcc6185a9c447ee275258ca0e261e6b5a
Idar Ulvestad Haugstuen <idaruh@gmail.com>
2020-10-25 09:37:36 +0100

    Adjustments for enabling usage of dockerfile in github action.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd terraform
-sed -i "s/TF_API_TOKEN/$1/g" .terraformrc
+sed -i "s/TF_API_TOKEN/$TF_API_TOKEN/g" .terraformrc
 mv .terraformrc $HOME
 terraform init
 terraform apply -auto-approve
